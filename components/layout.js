062293f9adedaeb525cef71e1c72f0a381216300
Emirhan Aksu <emirhanaksu0@hotmail.com>
2021-04-07 03:31:07 +0300

    Initial commit

---
+++ components/layout.js
@@ -0,0 +1,7 @@
+import Head from 'next/head';
+
+function Layout({ children }) {
+  return <div>{children}</div>;
+}
+
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Layout from "../components/layout.js";
+
+export default function Home() {
+  return (
+    <Layout>
+      <p className="text-blue-300">Welcome to Jetzlex personal website!</p>
+    </Layout>
+  );
+}
